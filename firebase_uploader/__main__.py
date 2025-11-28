277236592b1b680abdd9f9f1bb490071bb018f8e
Felipe <feliosphere@gmail.com>
2025-11-28 17:10:53 -0500

    chore: replace csv with pandas

--- firebase_uploader/__main__.py
+++ firebase_uploader/__main__.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2025 Felipe Paucar
 # Licensed under the MIT License
 
-from cli import cli_entrypoint
+from .cli import cli_entrypoint
 
 if __name__ == '__main__':
     cli_entrypoint()
